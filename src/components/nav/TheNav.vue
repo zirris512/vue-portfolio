d5a03939a8cff369e353f06e441022d50765fd7f
Brent <mina512@live.com>
2021-04-28 16:44:26 -0500

    fix dropdown events in nav

--- src/components/nav/TheNav.vue
+++ src/components/nav/TheNav.vue
@@ -1,53 +1,68 @@
 <template>
-    <nav class="h-16 fixed w-full top-0 left-0">
-        <ul
-            class="flex items-center h-full w-full bg-black text-2xl select-none"
-        >
-            <li class="flex-1"><a href="#">Brent Edwards</a></li>
-            <li><a href="#">About</a></li>
-            <li
-                class="relative cursor-pointer"
-                @click="showTitles = !showTitles"
-            >
-                <a tabindex="0"> Portfolio </a>
-                <font-awesome-icon
-                    v-if="!showTitles"
-                    icon="angle-down"
-                    class="text-white"
-                />
-                <font-awesome-icon v-else icon="angle-up" class="text-white" />
-                <ul
-                    class="bg-black absolute w-max mt-3 -left-5 text-lg"
-                    v-show="showTitles"
-                    @mouseleave="showTitles = false"
-                >
-                    <li class="w-full px-4 py-2 text-white">
-                        <a href="#">All</a>
-                    </li>
-                    <li
-                        class="w-full px-4 py-2 text-white"
-                        v-for="project in data"
+    <nav class="h-16 fixed w-full top-0 left-0 bg-black px-4">
+        <div class="flex items-center h-full w-full text-2xl select-none">
+            <a href="#">Brent Edwards</a>
+            <div class="ml-auto flex gap-10">
+                <a href="#">About</a>
+                <div class="relative" ref="dropdown" @click="toggleDropdown">
+                    <a href="#">
+                        Portfolio<font-awesome-icon
+                            v-if="!showTitles"
+                            icon="angle-down"
+                        /><font-awesome-icon v-else icon="angle-up" />
+                    </a>
+                    <div
+                        class="flex flex-col absolute -left-3 w-max px-3 bg-black"
+                        v-if="showTitles"
                     >
-                        <a href="#">{{ project.title }}</a>
-                    </li>
-                </ul>
-            </li>
-            <li><a href="#">Contact</a></li>
-        </ul>
+                        <a
+                            href="#"
+                            class="my-2"
+                            v-for="project in data"
+                            :key="project.title"
+                        >
+                            {{ project.title }}
+                        </a>
+                    </div>
+                </div>
+                <a href="#">Contact</a>
+            </div>
+        </div>
     </nav>
 </template>
 
 <script lang="ts">
-    import { defineComponent, ref } from "vue";
+    import { defineComponent, onBeforeUnmount, onMounted, ref } from "vue";
     import data from "../../data/projects";
 
     export default defineComponent({
         setup() {
             const showTitles = ref(false);
+            const dropdown = ref<HTMLDivElement | null>(null);
+
+            const toggleDropdown = () => {
+                showTitles.value = !showTitles.value;
+            };
+
+            const toggleClose = (event: Event) => {
+                if (!dropdown.value?.contains(event.target as HTMLElement)) {
+                    showTitles.value = false;
+                }
+            };
+
+            onMounted(() => {
+                document.addEventListener("click", toggleClose);
+            });
+
+            onBeforeUnmount(() => {
+                document.removeEventListener("click", toggleClose);
+            });
 
             return {
                 data,
+                dropdown,
                 showTitles,
+                toggleDropdown,
             };
         },
     });
@@ -60,9 +75,5 @@
 
     a:focus {
         outline: none;
-    }
-
-    ul li {
-        margin: 0 2%;
     }
 </style>
