7cd08905e77b83d9a1b70c72d17e4065dbb3c84e
Brent <mina512@live.com>
2021-04-28 14:53:09 -0500

    add footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
     purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        fontFamily: {
+            body: ["'Roboto'", "sans-serif"],
+        },
         extend: {},
     },
     variants: {
